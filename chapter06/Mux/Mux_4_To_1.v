34afc86ce21c70963326025950729b73930fc4f1
russell-merrick <37701986+russell-merrick@users.noreply.github.com>
2022-03-18 10:07:58 -0400

    consolidated verilog

--- chapter06/Mux/Mux_4_To_1.v
+++ chapter06/Mux/Mux_4_To_1.v
@@ -1,7 +1,8 @@
 module Mux_4_To_1 (input in1, input in2, input in3, input in4, 
                    input sel1, input sel2, output out);
  
-assign out = sel1 ? (sel2 ? in4 : in3) :
-                    (sel2 ? in2 : in1);
-
+assign out = !sel1 & !sel2 ? in1 :
+             !sel1 &  sel2 ? in2 :
+              sel1 & !sel2 ? in3 : in4;
+  
 endmodule
