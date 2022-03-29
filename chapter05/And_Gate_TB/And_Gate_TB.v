65104968814285cd4bf1dc7a39b00784e228b39c
russell-merrick <37701986+russell-merrick@users.noreply.github.com>
2022-03-28 20:42:12 -0400

    added simple testbench example in ch5

---
+++ chapter05/And_Gate_TB/And_Gate_TB.v
@@ -0,0 +1,32 @@
+// Simple testbench to exercise the And_Gate_Project
+module And_Gate_TB();
+  
+  reg r_In1, r_In2;
+  wire w_Out;
+  
+  // Here is where we instantiate the UUT
+  And_Gate_Project UUT 
+  (.i_Switch_1(r_In1),
+   .i_Switch_2(r_In2),
+   .o_LED_1(w_Out));
+  
+  // The steps to exercise the UUT go here
+  initial
+    begin
+      $dumpfile("dump.vcd"); $dumpvars;
+      r_In1 <= 1'b0;
+      r_In2 <= 1'b0;
+      #10;
+      r_In1 <= 1'b0;
+      r_In2 <= 1'b1;
+      #10;
+      r_In1 <= 1'b1;
+      r_In2 <= 1'b0;
+      #10;
+      r_In1 <= 1'b1;
+      r_In2 <= 1'b1;
+      #10;
+      #10;
+      $finish();
+    end
+endmodule
---
+++ chapter05/And_Gate_TB/And_Gate_TB.vhd
@@ -0,0 +1,41 @@
+-- Simple testbench to exercise the And_Gate_Project
+library IEEE;
+use IEEE.std_logic_1164.all;
+use std.env.finish;
+
+entity And_Gate_TB is
+end entity And_Gate_TB;
+
+architecture behave of And_Gate_TB is
+  signal r_In1, r_In2, w_Out : std_logic;
+
+begin
+ 
+  -- Here is where we instantiate the UUT
+  UUT : entity work.And_Gate_Project
+  port map (
+    i_Switch_1 => r_In1,
+    i_Switch_2 => r_In2,
+    o_LED_1    => w_Out);
+  
+  -- The steps to exercise the UUT go here
+  process is 
+  begin
+  	r_In1 <= '0';
+    r_In2 <= '0';
+    wait for 10 ns;
+    r_In1 <= '0';
+    r_In2 <= '1';
+    wait for 10 ns;
+    r_In1 <= '1';
+    r_In2 <= '0';
+    wait for 10 ns;
+    r_In1 <= '1';
+    r_In2 <= '1';
+    wait for 10 ns;
+    wait for 10 ns;
+    finish;
+  end process;
+  
+
+end behave;
