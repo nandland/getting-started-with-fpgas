2df687f84dd364046f718e63c8390aefc98a49be
russell-merrick <37701986+russell-merrick@users.noreply.github.com>
2022-05-19 12:43:20 -0400

    VHDL fixes and project path updates

--- chapter06/Demux_LFSR_Project_VHDL/Demux_LFSR_Project_Top.vhd
+++ chapter06/Demux_LFSR_Project_VHDL/Demux_LFSR_Project_Top.vhd
@@ -16,7 +16,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity Demux_Project_Top is 
+entity Demux_LFSR_Project_Top is 
   port (
     i_Clk      : in  std_logic;
     i_Switch_1 : in  std_logic;
@@ -25,9 +25,9 @@ entity Demux_Project_Top is
     o_LED_2    : out std_logic;
     o_LED_3    : out std_logic;
     o_LED_4    : out std_logic);
-end entity Demux_Project_Top;   
+end entity Demux_LFSR_Project_Top;   
 
-architecture RTL of Demux_Project_Top is
+architecture RTL of Demux_LFSR_Project_Top is
     
   -- LFSR takes 2^22-1 (4,194,303) clock cycles to complete
   constant NUM_LFSR_BITS : integer := 22;
@@ -38,7 +38,9 @@ architecture RTL of Demux_Project_Top is
   signal r_LFSR_Toggle : std_logic := '0';
   signal w_Slow_Pulse : std_logic;
   signal w_LED_Toggle, w_Counter_Toggle : std_logic;
-  
+
+begin
+
   Toggle_LFSR : entity work.LFSR 
   generic map (
     NUM_BITS => NUM_LFSR_BITS)
@@ -55,8 +57,9 @@ architecture RTL of Demux_Project_Top is
     if rising_edge(i_Clk) then
         if w_Slow_Pulse = '1' then
             r_LFSR_Toggle <= not r_LFSR_Toggle;
-    end
-  end
+        end if;
+    end if;
+  end process;
 
   Toggle_Counter : entity work.Count_And_Toggle 
   generic map (
@@ -77,71 +80,5 @@ architecture RTL of Demux_Project_Top is
     o_Data1 => o_LED_2,
     o_Data2 => o_LED_3,
     o_Data3 => o_LED_4);
-
-endmodule
-
--- Top of Mux Demonstration
--- Instantiates 4 LED blinkers at different frequencies
--- User can select which one to output by using S1 and S2
--- Demonstrates muxing of any of these to a single LED output (o_LED_1)
-
-
-
-entity Mux_Project_Top is
-    port (
-        i_Clk      : in  std_logic;
-        i_Switch_1 : in  std_logic;
-        i_Switch_2 : in  std_logic;
-        o_LED_1    : out std_logic);
-end entity Mux_Project_Top;
-
-architecture RTL of Mux_Project_Top is
-
-    signal w_Toggle_1Hz, w_Toggle_2Hz, w_Toggle_5Hz, w_Toggle_10Hz : std_logic;
-
-begin
-    
-    Toggle_1Hz : entity work.Count_And_Toggle 
-    generic map (
-        COUNT_LIMIT => 25000000/2)
-    port map (
-        i_Clk    => i_Clk,
-        i_Enable => '1',
-        o_Toggle => w_Toggle_1Hz);
-
-    Toggle_2Hz : entity work.Count_And_Toggle 
-    generic map (
-        COUNT_LIMIT => 25000000/4)
-    port map (
-        i_Clk    => i_Clk,
-        i_Enable => '1',
-        o_Toggle => w_Toggle_2Hz);
-
-    Toggle_5Hz : entity work.Count_And_Toggle 
-    generic map (
-        COUNT_LIMIT => 25000000/10)
-    port map (
-        i_Clk    => i_Clk,
-        i_Enable => '1',
-        o_Toggle => w_Toggle_5Hz);
-        
-    Toggle_10Hz : entity work.Count_And_Toggle 
-    generic map (
-        COUNT_LIMIT => 25000000/20)
-    port map (
-        i_Clk    => i_Clk,
-        i_Enable => '1',
-        o_Toggle => w_Toggle_10Hz);
-
-    Mux_4_To_1_Inst : entity work.Mux_4_To_1
-    port map (
-        in1  => w_Toggle_1Hz,
-        in2  => w_Toggle_2Hz,
-        in3  => w_Toggle_5Hz, 
-        in4  => w_Toggle_10Hz,
-        sel1 => i_Switch_1,
-        sel2 => i_Switch_2,
-        out1 => o_LED_1);
-          
 
 end architecture RTL;
