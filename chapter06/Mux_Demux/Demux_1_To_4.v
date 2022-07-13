cb9909e85e7dee28bea409852da540963378ca17
russell-merrick <37701986+russell-merrick@users.noreply.github.com>
2022-07-12 21:41:24 -0400

    formatting update

--- chapter06/Mux_Demux/Demux_1_To_4.v
+++ chapter06/Mux_Demux/Demux_1_To_4.v
@@ -1,13 +1,14 @@
 // Implements a 1-4 Demultiplexer.
 // In reality, it is unlikely you would put a demux in a dedicated module.
 
-module Demux_1_To_4 (input  i_Data,
-                     input  i_Sel1, 
-                     input  i_Sel0, 
-                     output o_Data0,
-                     output o_Data1,
-                     output o_Data2,
-                     output o_Data3);
+module Demux_1_To_4 
+ (input  i_Data,
+  input  i_Sel1, 
+  input  i_Sel0, 
+  output o_Data0,
+  output o_Data1,
+  output o_Data2,
+  output o_Data3);
  
 assign o_Data0 = !i_Sel1 & !i_Sel0 ? i_Data : 1'b0;
 assign o_Data1 = !i_Sel1 &  i_Sel0 ? i_Data : 1'b0;
