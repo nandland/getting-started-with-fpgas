c7876b447cf19689b0d75de738d39af844ee7bd7
russell-merrick <37701986+russell-merrick@users.noreply.github.com>
2022-10-01 11:00:37 -0400

    basic memory TB functional

--- chapter10/State_Machine_Project_VHDL/State_Machine_Game_TB.vhd
+++ chapter10/State_Machine_Project_VHDL/State_Machine_Game_TB.vhd
@@ -1,85 +1,78 @@
-// Testbench of State Machine Game
-// Simple tests to ensure that state machine works
+-- Testbench of State Machine Game
+-- Simple tests to ensure that state machine works
 
-module State_Machine_Game_TB ();
+library ieee;
+use ieee.std_logic_1164.all;
+use std.env.finish;
 
-  localparam CLKS_PER_SEC = 6;
+entity State_Machine_Game_TB is
+end State_Machine_Game_TB;
+
+architecture test of State_Machine_Game_TB is
+  constant CLKS_PER_SEC : integer := 6;
+  constant GAME_LIMIT   : integer := 3;
+  signal r_Clk : std_logic := '0';
+  signal r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4 : std_logic := '0';
+  signal w_LED_1, w_LED_2, w_LED_3, w_LED_4 : std_logic;
+  signal w_Score : std_logic_vector(3 downto 0);
+
+  -- Sets switches to desired value for one clock cycle, 
+  -- then drives back to 0.
+  procedure set_switches (
+    constant i_1, i_2, i_3, i_4 : in std_logic;
+    signal   o_1, o_2, o_3, o_4 : out std_logic) is
+  begin
+    wait until rising_edge(r_Clk);
+    o_1 <= i_1;
+    o_2 <= i_2;
+    o_3 <= i_3;
+    o_4 <= i_4;
+    wait until rising_edge(r_Clk);
+    o_1 <= '0';
+    o_2 <= '0';
+    o_3 <= '0';
+    o_4 <= '0';
+    wait until rising_edge(r_Clk);
+  end set_switches;
+
+begin
+
+  r_Clk <= not r_Clk after 10 ns;
   
-  reg r_Clk = 1'b0;
-  reg r_Switch_1 = 1'b0, r_Switch_2 = 1'b0, r_Switch_3 = 1'b0, r_Switch_4 = 1'b0;
-  wire w_LED_1, w_LED_2, w_LED_3, w_LED_4;
-  wire [7:0] w_Score;
-
-  always #2 r_Clk <= !r_Clk;
-
-
-  State_Machine_Game #(.CLKS_PER_SEC(CLKS_PER_SEC),
-                       .GAME_LIMIT(3)) Game_Inst
-   (.i_Clk(r_Clk),
-    .i_Switch_1(r_Switch_1),
-    .i_Switch_2(r_Switch_2),
-    .i_Switch_3(r_Switch_3),
-    .i_Switch_4(r_Switch_4),
-    .o_Score(w_Score),
-    .o_LED_1(w_LED_1),
-    .o_LED_2(w_LED_2),
-    .o_LED_3(w_LED_3),
-    .o_LED_4(w_LED_4));
-
-  // Sets switches to desired value for one clock cycle, 
-  // then drives back to 0.
-  task set_switches;
-    input i_1, i_2, i_3, i_4;
-    begin
-      @(posedge r_Clk);
-      r_Switch_1 <= i_1;
-      r_Switch_2 <= i_2;
-      r_Switch_3 <= i_3;
-      r_Switch_4 <= i_4;
-      @(posedge r_Clk);
-      r_Switch_1 <= 1'b0;
-      r_Switch_2 <= 1'b0;
-      r_Switch_3 <= 1'b0;
-      r_Switch_4 <= 1'b0;
-      @(posedge r_Clk);
-    end
-  endtask
+  Game_Inst : entity work.State_Machine_Game
+    generic map (
+      CLKS_PER_SEC => CLKS_PER_SEC,
+      GAME_LIMIT   => GAME_LIMIT) 
+    port map ( 
+      i_Clk      => r_Clk,
+      i_Switch_1 => r_Sw_1,
+      i_Switch_2 => r_Sw_2,
+      i_Switch_3 => r_Sw_3,
+      i_Switch_4 => r_Sw_4,
+      o_Score    => w_Score,
+      o_LED_1    => w_LED_1,
+      o_LED_2    => w_LED_2,
+      o_LED_3    => w_LED_3,
+      o_LED_4    => w_LED_4);
   
-  
-  initial begin
-    // Required for EDA Playground
-    $dumpfile("dump.vcd"); 
-    $dumpvars;
-    set_switches(0, 0, 0, 0);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    set_switches(1, 1, 0, 0);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    set_switches(0, 0, 0, 1);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    set_switches(0, 0, 0, 1);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    set_switches(0, 0, 0, 1);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-
-    // Reset game back to start
-    set_switches(1, 1, 0, 0);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-       
-    $display("Test Complete");
-    $finish();
-  end
+  process is
+  begin
+    set_switches('0', '0', '0', '0', r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4);
+    wait for 180 ns;
+    set_switches('1', '1', '0', '0', r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4);
+    wait for 180 ns;
+    set_switches('0', '0', '0', '1', r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4);
+    wait for 180 ns;
+    set_switches('0', '0', '0', '1', r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4);
+    wait for 180 ns;
+    set_switches('0', '0', '0', '1', r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4);
+    wait for 180 ns;
+    
+    -- Reset game back to start
+    set_switches('1', '1', '0', '0', r_Sw_1, r_Sw_2, r_Sw_3, r_Sw_4);
+    wait for 60 ns;
    
-endmodule
+    finish;  -- need VHDL-2008
+  end process;
+   
+end test;
--- chapter10/State_Machine_Project_Verilog/State_Machine_Game_TB.v
+++ chapter10/State_Machine_Project_Verilog/State_Machine_Game_TB.v
@@ -4,17 +4,17 @@
 module State_Machine_Game_TB ();
 
   localparam CLKS_PER_SEC = 6;
-  
+  localparam GAME_LIMIT = 3;
   reg r_Clk = 1'b0;
-  reg r_Switch_1 = 1'b0, r_Switch_2 = 1'b0, r_Switch_3 = 1'b0, r_Switch_4 = 1'b0;
+  reg r_Switch_1 = 1'b0, r_Switch_2 = 1'b0;
+  reg r_Switch_3 = 1'b0, r_Switch_4 = 1'b0;
   wire w_LED_1, w_LED_2, w_LED_3, w_LED_4;
-  wire [7:0] w_Score;
+  wire [3:0] w_Score;
 
   always #2 r_Clk <= !r_Clk;
 
-
   State_Machine_Game #(.CLKS_PER_SEC(CLKS_PER_SEC),
-                       .GAME_LIMIT(3)) Game_Inst
+                       .GAME_LIMIT(GAME_LIMIT)) Game_Inst
    (.i_Clk(r_Clk),
     .i_Switch_1(r_Switch_1),
     .i_Switch_2(r_Switch_2),
@@ -45,38 +45,24 @@ module State_Machine_Game_TB ();
     end
   endtask
   
-  
   initial begin
     // Required for EDA Playground
     $dumpfile("dump.vcd"); 
     $dumpvars;
     set_switches(0, 0, 0, 0);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
+    repeat(CLKS_PER_SEC) @(posedge r_Clk);
     set_switches(1, 1, 0, 0);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
+    repeat(2*CLKS_PER_SEC) @(posedge r_Clk);
     set_switches(0, 0, 0, 1);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
+    repeat(3*CLKS_PER_SEC) @(posedge r_Clk);
     set_switches(0, 0, 0, 1);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
+    repeat(2 * CLKS_PER_SEC) @(posedge r_Clk);
     set_switches(0, 0, 0, 1);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-
+    repeat(2 * CLKS_PER_SEC) @(posedge r_Clk);
+    
     // Reset game back to start
     set_switches(1, 1, 0, 0);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
-    repeat(CLKS_PER_SEC/2) @(posedge r_Clk);
+    repeat(2*CLKS_PER_SEC) @(posedge r_Clk);
        
     $display("Test Complete");
     $finish();
